7bbf081ffffeb3e5184c9272bc2941e87edf01aa
hyroky867 <handm755@gmail.com>
2024-01-13 14:06:54 +0900

    fix: to ts file

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,16 +1,23 @@
-const { getStoryContext } = require("@storybook/test-runner");
-const { injectAxe, checkA11y, configureAxe } = require("axe-playwright");
+import { checkA11y, configureAxe, injectAxe } from 'axe-playwright';
+import { getStoryContext } from '@storybook/test-runner';
+import { Page } from 'playwright';
 
-module.exports = {
-  async preRender(page, context) {
-    if (context.name.startsWith("SP")) {
+type Context = {
+  id: string;
+  title: string;
+  name: string;
+};
+
+export default {
+  async preVisit(page: Page, context: Context) {
+    if (context.name.startsWith('SP')) {
       page.setViewportSize({ width: 375, height: 667 });
     } else {
       page.setViewportSize({ width: 1280, height: 800 });
     }
     await injectAxe(page);
   },
-  async postRender(page, context) {
+  async postRender(page: Page, context: Context) {
     const storyContext = await getStoryContext(page, context);
     if (storyContext.parameters?.a11y?.disable) {
       return;
@@ -18,8 +25,8 @@ module.exports = {
     await configureAxe(page, {
       rules: storyContext.parameters?.a11y?.config?.rules,
     });
-    await checkA11y(page, "#root", {
-      includedImpacts: ["critical", "serious"],
+    await checkA11y(page, '#root', {
+      includedImpacts: ['critical', 'serious'],
       detailedReport: false,
       detailedReportOptions: { html: true },
       axeOptions: storyContext.parameters?.a11y?.options,
--- src/services/client/MyProfile/__mock__/msw.ts
+++ src/services/client/MyProfile/__mock__/msw.ts
@@ -1,13 +1,13 @@
 import { rest } from 'msw';
 
-import { getMyProfileData } from './fixture';
 import { path } from '..';
+import { getMyProfileData } from './fixture';
 
-export function handleGetMyProfile(args?: {
+export const handleGetMyProfile = (args?: {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   mock?: jest.Mock<any, any>;
   status?: number;
-}) {
+}) => {
   return rest.get(path(), async (_, res, ctx) => {
     args?.mock?.();
     if (args?.status) {
@@ -15,6 +15,6 @@ export function handleGetMyProfile(args?: {
     }
     return res(ctx.status(200), ctx.json(getMyProfileData));
   });
-}
+};
 
 export const handlers = [handleGetMyProfile()];
