b72fce4f8a97321ee20321eca86a4b3ed002e738
hyroky867 <handm755@gmail.com>
2024-02-17 16:07:55 +0900

    fix: integrate storybook

--- src/components/molecules/TextareaWithInfo/index.stories.tsx
+++ src/components/molecules/TextareaWithInfo/index.stories.tsx
@@ -1,10 +1,13 @@
 import { Meta, StoryObj } from '@storybook/react';
+import { expect, within } from '@storybook/test';
 
 import { TextareaWithInfo } from './';
 
 export default {
   component: TextareaWithInfo,
-  args: { title: '記事本文' },
+  args: {
+    title: '記事本文',
+  },
 } satisfies Meta<typeof TextareaWithInfo>;
 
 type Story = StoryObj<typeof TextareaWithInfo>;
@@ -12,15 +15,21 @@ type Story = StoryObj<typeof TextareaWithInfo>;
 export const Default: Story = {};
 
 export const Info: Story = {
-  args: { info: '0 / 64' },
+  args: {
+    info: '0 / 64',
+  },
 };
 
 export const Description: Story = {
-  args: { description: '不正な文字が含まれています' },
+  args: {
+    description: '不正な文字が含まれています',
+  },
 };
 
 export const Error: Story = {
-  args: { error: '不正な文字が含まれています' },
+  args: {
+    error: '不正な文字が含まれています',
+  },
 };
 
 export const FullProps: Story = {
@@ -28,5 +37,17 @@ export const FullProps: Story = {
     info: '0 / 64',
     description: '半角英数64文字以内で入力してください',
     error: '不正な文字が含まれています',
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const textbox = canvas.getByRole('textbox', {
+      name: '記事本文',
+    });
+    await expect(textbox).toHaveAccessibleDescription(
+      '半角英数64文字以内で入力してください',
+    );
+    await expect(textbox).toHaveAccessibleErrorMessage(
+      '不正な文字が含まれています',
+    );
   },
 };
