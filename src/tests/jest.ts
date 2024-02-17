cb527c82c93b82b5f10c843d7bbd5357fcd1c6bd
hyroky867 <handm755@gmail.com>
2024-02-17 16:12:07 +0900

    feat: section-29

--- src/tests/jest.ts
+++ src/tests/jest.ts
@@ -4,13 +4,13 @@ import { setupServer } from 'msw/node';
 
 import type { RequestHandler } from 'msw';
 
-export function setupMockServer(...handlers: RequestHandler[]) {
+export const setupMockServer = (...handlers: RequestHandler[]) => {
   const server = setupServer(...handlers);
   beforeAll(() => server.listen());
   afterEach(() => server.resetHandlers());
   afterAll(() => server.close());
   return server;
-}
+};
 
 export function selectImageFile(
   inputTestId = 'file',
